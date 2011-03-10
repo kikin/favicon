1dd9847bcf9cf56807a20783113dfc62a988b91c
sandesh.devaraju@kikin.com <sandesh.devaraju@kikin.com@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-03-10 17:06:26 +0000

    use virtualenv
    git-svn-id: https://mykikin.jira.com/svn/SBX/favicon/trunk@21299 e6edf6fb-f266-4316-afb4-e53d95876a76

--- start.py
+++ start.py
@@ -1,17 +1,25 @@
-import os.path
 import sys
-
-import atexit
-import threading
-import cherrypy
+import site
 
 sys.stdout = sys.stderr
 
 ROOT='/opt/favicon_env'
 
-sys.path.append(os.path.join(ROOT, 'src'))
-import favicon
+prev_sys_path = list(sys.path)
 
+# Add site-packages of our virtualenv as a site directory
+site.addsitedir(ROOT + '/venv/lib/python2.6/site-packages/')
+# Add application directory to PYTHONPATH
+sys.path.append(ROOT + '/src')
+
+# Reorder sys.path so added directories take precedence
+new_sys_path = [p for p in sys.path if p not in prev_sys_path]
+for item in new_sys_path:
+  sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+import cherrypy
+import atexit
 from logging import handlers, INFO
 
 # Remove the default FileHandlers if present.
@@ -20,7 +28,7 @@ cherrypy.log.error_file = ''
 # Make a new RotatingFileHandler for the error log.
 err_fname = getattr(cherrypy.log,
                     'rot_error_file',
-                    os.path.join(ROOT, 'logs/errorLog'))
+                    ROOT + '/logs/errorLog')
 
 err_handler = handlers.TimedRotatingFileHandler(err_fname, 'midnight', 1, 7)
 err_handler.setLevel(INFO)
@@ -29,15 +37,16 @@ err_handler.setFormatter(cherrypy._cplogging.logfmt)
 cherrypy.log.error_log.addHandler(err_handler)
 
 # Load config
-config = os.path.join(ROOT, 'src/prod.conf')
+config = ROOT + '/src/prod.conf'
 cherrypy.config.update(config)
-cherrypy.config.update({'favicon.root' : os.path.join(ROOT, 'src')})
+cherrypy.config.update({'favicon.root' : ROOT + '/src'})
 
 if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
   cherrypy.engine.start(blocking=False)
   atexit.register(cherrypy.engine.stop)
 
+import favicon
+
 application = cherrypy.Application(favicon.PrintFavicon(),
                                    script_name=None,
                                    config=config)
-
