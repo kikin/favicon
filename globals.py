c42431441d56d61a809a3a19ad2307cb74b30430
ehtesh.choudhury <ehtesh.choudhury@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-08-23 19:35:00 +0000

    added RE_METAREFRESH , changed flags to a keyword parameter
    git-svn-id: https://mykikin.jira.com/svn/SBX/favicon/trunk@24005 e6edf6fb-f266-4316-afb4-e53d95876a76

--- globals.py
+++ globals.py
@@ -13,8 +13,9 @@ MC_CACHE_TIME = 2419200 # seconds (28 days)
 
 KEY_FORMAT = 'icon_loc-%s'
 
-RE_URLDECODE = re.compile('%([0-9a-hA-H][0-9a-hA-H])', re.MULTILINE)
-RE_LINKTAG = re.compile('^(shortcut|icon|shortcut icon)$', re.IGNORECASE)
+RE_URLDECODE = re.compile('%([0-9a-hA-H][0-9a-hA-H])', flags=re.MULTILINE)
+RE_LINKTAG = re.compile('^(shortcut|icon|shortcut icon)$', flags=re.IGNORECASE)
+RE_METAREFRESH = re.compile('url=([^;]+)', flags=re.IGNORECASE)
 
 DEFAULT_FAVICON_LOC = 'http://d3gibmfbqm9w63.cloudfront.net/img/static/default_favicon.png'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; ' +
